fd83ae899ab25e99965a4426effb402650d9340f
sean <sean.hoyal@external.eodc.eu>
2024-06-26 14:10:37 +0200

    add workflow

--- openeo_argoworkflows/api/openeo_argoworkflows_api/workflows.py
+++ openeo_argoworkflows/api/openeo_argoworkflows_api/workflows.py
@@ -16,3 +16,14 @@ def hello_world(service: WorkflowsService, process_graph: dict):
         with Steps(name="steps"):
             print_graph(arguments={"graph": process_graph})
     return w
+
+def hello_world(service: WorkflowsService, process_graph: dict):
+    with Workflow(
+        generate_name="openeo-executor-",
+        entrypoint="steps",
+        namespace="default",
+        workflows_service=service,
+    ) as w:
+        with Steps(name="steps"):
+            print_graph(arguments={"graph": process_graph})
+    return w
